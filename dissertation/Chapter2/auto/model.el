860e74a6f1e69c48ebcd6841e51c63395ade8a50
Anh Le <anh.le91@gmail.com>
2018-01-09 20:42:51 -0500

    WRITE more writing, start model description

---
+++ dissertation/Chapter2/auto/model.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook
+ "model"
+ (lambda ()
+   (LaTeX-add-labels
+    "chap:model"
+    "eq:prob_offer_ij"
+    "eq:conditional_probability_of_offer"
+    "eq:conditional_probability_of_accept"
+    "eq:updateO_joint_dist_into_conditional_dist"
+    "eq:updatealpha_MHratio_final"
+    "eq:updatebeta_MHratio_simplify"
+    "eq:updatebeta_MHratio_final"))
+ :latex)
+
